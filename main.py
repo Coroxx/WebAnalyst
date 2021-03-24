7afcdb7920cc89f008513aa713f6e59422bb9985
Coroxx <unnainstituteur@gmail.com>
2021-03-24 09:46:38 +0100

    wip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import time
 import os
+import re
+import whois
 
 # coding : utf-8
 
@@ -20,7 +22,7 @@ COLORS = {
     "cyan-background": "\u001b[46;1m",
 }
 
-asciidraw = "\n[[lightyellow]] _ _ _     _      _____         _         _   \n| | | |___| |_   |  _  |___ ___| |_ _ ___| |_ \n| | | | -_| . |  |     |   | .'| | | |_ -|  _|\n|_____|___|___|  |__|__|_|_|__,|_|_  |___|_|  \n                                 |___|        \nAuthor : @Coroxx on github\n\n"
+asciidraw = "\n[[lightyellow]] _ _ _     _      _____         _         _   \n| | | |___| |_   |  _  |___ ___| |_ _ ___| |_ \n| | | | -_| . |  |     |   | .'| | | |_ -|  _|\n|_____|___|___|  |__|__|_|_|__,|_|_  |___|_|  \n                                 |___|        \nAuthor : @Coroxx on github\n"
 
 
 def colorText(text):
@@ -34,10 +36,9 @@ def lang():
     global asciidraw
     os.system("clear")
     response = input(
-        colorText("[[yellow]]Language :\n\n[1] FR\n[2] ANG \n\nChoice : "))
+        colorText("[[yellow]]Language/Langue :\n\n[1] French\n[2] English \n\nChoice : "))
     if response == "1":
         lang = "fr"
-        asciidraw += "[1] Analyser une page web\n\n[2] Quitter"
     elif response == "2":
         lang = "ang"
     else:
@@ -85,15 +86,112 @@ except:
 
 
 def parameters_FR():
-    pass
+    settings = []
+    global request
+    link = str(input(colorText(
+        "[[white]]\nLien de la page (doit commencer par https/http): ")))
+    if (bool(re.match(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=])*', link))):
+        try:
+            request = requests.get(link, timeout=5)
+            print(colorText('[[green]][+] Ajouté !'))
+            settings.append(link)
+        except:
+            print(colorText('\n[[red]][-] Le site ne réponds pas..'))
+            time.sleep(2)
+            parameters_FR()
+    else:
+        print(colorText('[[red]][!] Syntaxe incorrecte !'))
+        time.sleep(2)
+        parameters_FR()
+    whoisresult = input(colorText(
+        '[[yellow]]\n[?] Voulez-vous analyser le domaine automatiquement à l\'aide de whois ? (y/n): '))
+    if (bool(re.match(r"OUI|oui|y(?:es)?|Y", whoisresult))):
+        settings.append(True)
+    elif (bool(re.match(r"N(?:ON)?|n(?:on?)?", whoisresult))):
+        settings.append(False)
+    time.sleep(2)
+    print(colorText('\n[[green]][+] Démarrage de l\'analyse...'))
+
+    result = parser(link, whois)
+
+    time.sleep(1)
+
+    os.system('clear')
+    if whoisresult:
+        whoisresult = whois.query(link)
+        print(colorText('[[blue]]Whois : '),
+              '\n\nDate d\'expiration du domaine :', whoisresult.expiration_date,
+              '\nDate de création du domaine :', whoisresult.creation_date,
+              '\nNom : ', whoisresult.registrar,
+              '\nDernière mise à jour : ', whoisresult.last_updated)
+
+    time.sleep(2)
+    print(colorText('[[green]]--Résultats--'),
+          '\n\n\n Nombre de titres : ', result[3],
+          '\nNombre de liens : ', result[1],
+          '\nNombre d\'image : ', result[2],
+          '\nNombre de div : ', result[0],
+          '\nNombre de formulaires : ', result[4])
+    time.sleep(2)
+    again = input(colorText(
+        '[[yellow]]\n[?] Voulez-vous analyser un nouveau domaine ? (y/n): '))
+    if (bool(re.match(r"OUI|oui|y(?:es)?|Y", again))):
+        parameters_FR()
+    elif (bool(re.match(r"N(?:ON)?|n(?:on?)?", again))):
+        os.system('clear')
+        print(colorText('\n[[red]][!] Retour au terminal... '))
+        sys.exit()
 
 
 def parameters_US():
-    pass
+    global request
+    link = str(input(colorText(
+        "[[white]]\nPage link (must start with https/http): ")))
+    if (bool(re.match(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=])*', link))):
+        try:
+            request = requests.get(link, timeout=5)
+            print(colorText('[[green]][+] Succes !'))
+        except:
+            print(colorText('\n[[red]][-] The site has timeout ..'))
+            time.sleep(2)
+            parameters_US()
+    else:
+        print(colorText('[[red]][!] Incorrect syntax !'))
+        time.sleep(2)
+        parameters_US()
+    whois = input(colorText(
+        '[[yellow]]\n[?] Do you want to scan the domain using whois? (y/n): '))
+    if (bool(re.match(r"OUI|oui|y(?:es)?|Y", whois))):
+        whois = True
+    elif (bool(re.match(r"N(?:ON)?|n(?:on?)?", whois))):
+        whois = False
+    else:
+        print(colorText('[[red]][!] Incorrect choice'))
+        time.sleep(2)
+        parameters_US()
+    time.sleep(2)
+    print(colorText('\n[[green]][+] Starting the analysis...'))
+    time.sleep(1)
+
+    parser(link, whois)
+
+    return link
 
 
-def parser(url):
-    pass
+def parser(url, whois):
+    global request
+    counts = []
+
+    counts.append((request.text.count('<div')))
+    counts.append((request.text.count('<a')))
+    counts.append((request.text.count('<img')))
+    counts.append((request.text.count('<h')))
+    counts.append((request.text.count('<form')))
+    if '<script' in request.text:
+        counts.append(True)
+    else:
+        counts.append(False)
+    return counts
 
 
 def main_ANG():
@@ -103,7 +201,7 @@ def main_ANG():
 def main_FR():
     global asciidraw
     os.system("clear")
-    print(colorText(asciidraw))
+    print(colorText(asciidraw), "\n[1] Analyser une page web\n\n[2] Quitter")
     choice = input("\nChoix : ")
     try:
         choice = int(choice)
@@ -124,3 +222,5 @@ def main_FR():
 
 
 main_FR()
+
+print()
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-import time
-
-print('coucou')
-time.sleep(10)
-print('re')
+from ipwhois import IPWhois
+import socket
+dd = socket.gethostbyname('www.ecoledirecte.com')
+obj = IPWhois(dd)
+res = obj.lookup()
+print(res["nets"][0]['country'])
